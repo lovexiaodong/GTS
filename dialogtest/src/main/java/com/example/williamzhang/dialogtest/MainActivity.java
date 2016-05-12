70428ad9e5974608ff1be27d9ef30bc7d559d001
William Zhang <zydqlg@sina.com>
2016-05-12 19:24:32 +0800

    Create a simple dialog

---
+++ dialogtest/src/main/java/com/example/williamzhang/dialogtest/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.williamzhang.dialogtest;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void createDialog(View view)
+    {
+//        new AlertDialog.Builder(this)
+//                .setTitle("New Dialog")
+//                .setMessage("This is a dialog")
+//                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                Toast.makeText(MainActivity.this,"The button is ok", Toast.LENGTH_SHORT).show();
+//            }
+//        }).show();
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View myLoginView = layoutInflater.inflate(R.layout.simple_dialog_layout, null);
+        new AlertDialog.Builder(this).setView(myLoginView).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
